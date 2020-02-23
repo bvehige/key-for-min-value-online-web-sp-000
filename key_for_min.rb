865fd83be529386c57f3fbc8463e5d640f6ed81e
Brian Vehige <brianvehige@gmail.com>
2020-02-23 21:57:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,7 @@
 
 def key_for_min_value(name_hash)
   current_min_value = nil
+  current_min_item = nil 
   name_hash.each do |item, value|
     if item[value] > current_min_value 
       current_min_value = value 
